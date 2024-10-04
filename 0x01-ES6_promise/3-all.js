725fb44fed022de66545e92c694802eb604d611c
Eugene Agesa <eugenethebandit@gmail.com>
2024-10-04 13:49:23 -0700

    0x01. ES6 Promises tasksa complete

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+  return Promise.all([promise1, promise2])
+    .then((value) => console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`),
+      () => console.log('Signup system offline'));
+}
