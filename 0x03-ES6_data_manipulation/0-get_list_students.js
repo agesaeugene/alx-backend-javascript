8967675737a8171ba4d3e339bb6b1bcb9e60ff67
Eugene Agesa <eugenethebandit@gmail.com>
2024-10-08 05:46:38 -0700

    returning students ids in an  array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,10 +2,10 @@
  * Retrieving a list
  * 
  */
-export default function getListStudents() {
-	return [
+const getListStudents = () => [
 	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 	{ id: 2, firstName: 'James', location: 'Columbia' },
 	{ id: 5, firstName: 'Serena', location: 'San Francisco' },
-	];
-}	  
+      ];
+
+export default getListStudents;
