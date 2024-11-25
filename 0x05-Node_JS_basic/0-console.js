78b19b109d63dbc652b2248458b5ecfc9d1b0e0d
Eugene Agesa <eugenethebandit@gmail.com>
2024-11-25 21:17:15 +0000

    type script tasks

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * A message is displayed on the STDOUT
+ * @param Message strring to be displayes
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
